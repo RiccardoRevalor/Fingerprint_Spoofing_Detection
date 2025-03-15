f7d8d4e69b8c60a994c94fe78f071da02d9a3490
RiccardoRevalor <riccardorevalor@gmail.com>
2025-03-16 00:33:02 +0100

    feat:trained and fitted LDA 1 direction classifier, worked with Threshold Optimization techniques (GridSearch, ROC+Youndex Index) to optimize the model

--- plot_functions.py
+++ plot_functions.py
@@ -62,3 +62,88 @@ def densityHistograms(X, n_components, y=None,labelColors=None, classNames=None,
 
 
 
+
+def densityPlots_TR_VAL(xTR, yTR, xVAL, yVAL, title1=None, bins1=None, title2=None, bins2=None, title=None, xlabel="LDA direction", labelColors=None, classLabels=None, rows=2, cols=1):
+    #I select feature i and feature j, with i different from j and plot them on the 2 axis of every scatter plot chart
+
+
+    #subplot creation
+    fig, plots = plt.subplots(nrows=rows, ncols=cols, figsize=(cols*6, rows*4))
+    plots = plots.flatten()   #the 2D axes array becomes a 1D array in order to access each ax in a more straighforward way during the loop
+
+    subplot = plots[0]
+    for label in labelColors:
+        sample_with_that_class = xTR[0, yTR == label]
+        subplot.hist(x=sample_with_that_class, color=labelColors[label], alpha= 0.7, density=True, label=f"{classLabels[label]}", edgecolor="black", bins=bins1)
+        subplot.legend()
+        if title1:
+            subplot.set_title(title1)
+        subplot.set_xlabel(xlabel)
+        subplot.set_ylabel("Density")
+
+
+    subplot = plots[1]
+    for label in labelColors:
+        sample_with_that_class = xVAL[0, yVAL == label]
+        subplot.hist(x=sample_with_that_class, color=labelColors[label], alpha= 0.7, density=True, label=f"{classLabels[label]}", edgecolor="black", bins=bins2)
+        subplot.legend()
+        if title2:
+            subplot.set_title(title2)
+        subplot.set_xlabel(xlabel)
+        subplot.set_ylabel("Density")
+
+    plt.tight_layout(pad=3) #add padding between subplots to distance between eachother
+    if title:
+        fig.suptitle(title, fontsize=16)  # General title
+    plt.show()
+
+
+
+
+# m is the number of dimensions
+def plotSingle(D, L, m):
+    D0 = D[:, L == 0]
+    D1 = D[:, L == 1]
+    print(m)
+    for i in range(m):
+        plt.figure()
+        plt.xlabel("Feature " + str(i))
+        plt.ylabel("Number of elements")
+        plt.hist(D0[i, :], bins=60,density=True, alpha=0.7, label="Spoofed fingerprint")
+        plt.hist(D1[i, :], bins=60, density=True, alpha=0.7, label="Authentic fingerprint")
+        plt.legend()
+        plt.show()
+
+
+def plotTot(D, L, m):
+    D0 = D[:, L == 0]
+    D1 = D[:, L == 1]
+
+    plt.figure()
+    plt.xlabel("Feature " )
+    plt.ylabel("Number of elements")
+    plt.hist(D0[:, :], density=True, alpha=0.7, label="Spoofed fingerprint")
+    plt.hist(D1[:, :], density=True, alpha=0.7, label="Authentic fingerprint")
+    plt.legend()
+    plt.show()
+
+
+# m is the number of dimensions
+def plotCross(D, L,m):
+    D0 = D[:, L == 0]
+    D1 = D[:, L == 1]
+
+    for i in range(m):
+        for j in range(m):
+            if i == j:
+                continue
+            plt.figure()
+            plt.xlabel("Feature " + str(i))
+            plt.ylabel("Feature " + str(j))
+            plt.scatter(D0[i, :], D0[j, :], label="Spoofed fingerprint")
+            plt.scatter(D1[i, :], D1[j, :], label="Authentic fingerprint")
+            plt.legend()
+            plt.show()
+
+
+
